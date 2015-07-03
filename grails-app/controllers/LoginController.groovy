a57d1834662997412909ba8f5a540bf2e15a544e
Rohit Kumar <rohit.kumar@intelligrape.com>
2015-07-03 21:04:53 +0530

    migrated the test app to the grails3

--- grails-app/controllers/LoginController.groovy
+++ grails-app/controllers/LoginController.groovy
@@ -1,20 +1,24 @@
+import org.grails.plugin.jcaptcha.JcaptchaService
+
 class LoginController {
 
-    def jcaptchaService
+    JcaptchaService jcaptchaService
 
     def index = {}
 
     def image = {
+        println session.id
         if (params.submit) {
             flash.message = (jcaptchaService.validateResponse("image", session.id, params.response)) ? "Text matches" : "Text doesn't match"
         }
+        render view: 'image'
     }
 
     def sound = {
         if (params.submit) {
             flash.message = (jcaptchaService.validateResponse("sound", session.id, params.response)) ? "Text matches" : "Text doesn't match"
         }
+        render view: 'sound'
     }
-
 }
 
